883b6166d24dcf0973a2b6a7804367f9d2b3a9b8
parklor <parklor1013@gmail.com>
2026-04-23 12:10:58 +0800

    20260422

--- web.py
+++ web.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>羅婉薰Python網頁20260409</h1>"
+    homepage = "<h1>羅婉薰Python網頁20260422</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=wanxun>傳送使用者暱稱</a><br>"
@@ -69,7 +69,6 @@ def movie1():
                     movie_url = "https://www.atmovies.com.tw" + a_tag.get("href")
                     img_src = "https://www.atmovies.com.tw" + img_tag.get("src")
                     
-                
                     R += f'''
                         <div style="margin-bottom: 40px;">
                             <h3>{movie_name}</h3>
