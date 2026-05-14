a3b8733f13d484c7fc5d824559b1639d63f973b8
parklor <parklor1013@gmail.com>
2026-05-14 12:01:46 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -67,7 +67,7 @@ def webhook():
         info = "我是羅婉薰開發的電影聊天機器人，您選擇的電影分級是：" + rate + "，相關電影：\n"
         
         db = firestore.client()
-        collection_ref = db.collection("電影含分級")
+        collection_ref = db.collection("本週新片含分級")
         docs = collection_ref.get()
         
         result = ""
